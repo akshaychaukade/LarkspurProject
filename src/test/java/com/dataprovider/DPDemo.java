567b22ec533e465e4a1d01a0607bcee0f95dcd26
akshaychaukade <akshaychaukade1997@gmail.com>
2022-09-15 13:30:58 +0530

    Akshay

--- src/test/java/com/dataprovider/DPDemo.java
+++ src/test/java/com/dataprovider/DPDemo.java
@@ -2,9 +2,9 @@ package com.dataprovider;
 
 import org.testng.annotations.Test;
 
-public class DPDemo extends StateData{
-@Test(dataProvider="FirstDp",dataProviderClass=StateData.class)
-  public void m1(String statecode,String statename) {
-      System.out.println("State code"+statecode+"\t"+statename);
-}
+public class DPDemo extends StateData {
+	@Test(dataProvider = "FirstDp", dataProviderClass = StateData.class)
+	public void m1(String statecode, String statename) {
+		System.out.println("State code" + statecode + "\t" + statename);
+	}
 }
